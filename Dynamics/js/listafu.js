49fede16acf38beb46d8a2098400b51d75292fc0
ururawis <324102374@alumno.enp.unam.mx>
2025-06-16 20:54:51 -0600

    Agregué lo extra

--- Dynamics/js/listafu.js
+++ Dynamics/js/listafu.js
@@ -9,7 +9,72 @@ function agregartarea(){
     contenedor.appendChild(tarea); 
     console.log("Creaste una nueva tarea, que horror :(");  
 }
+function agregarensayo(){
 
+    let tarea = document.createElement('div');
+    tarea.textContent = "Ensayo";
+
+    tarea.className = "nuevatarea";
+
+    let contenedor = document.getElementById("contenedor");
+    contenedor.appendChild(tarea); 
+    console.log("Creaste una tarea tipo ensayo");  
+}
+function agregarcuestionario(){
+
+    let tarea = document.createElement('div');
+    tarea.textContent = "Cuestionario";
+
+    tarea.className = "nuevatarea";
+
+    let contenedor = document.getElementById("contenedor");
+    contenedor.appendChild(tarea); 
+    console.log("Creaste una tarea tipo cuestionario");  
+}
+function agregarficha(){
+
+    let tarea = document.createElement('div');
+    tarea.textContent = "Ficha";
+
+    tarea.className = "nuevatarea";
+
+    let contenedor = document.getElementById("contenedor");
+    contenedor.appendChild(tarea); 
+    console.log("Creaste una tarea tipo Ficha");  
+}
+function agregarreporte(){
+
+    let tarea = document.createElement('div');
+    tarea.textContent = "Reporte";
+
+    tarea.className = "nuevatarea";
+
+    let contenedor = document.getElementById("contenedor");
+    contenedor.appendChild(tarea); 
+    console.log("Creaste una tarea tipo Reprte");  
+}
+function agregaresquema(){
+
+    let tarea = document.createElement('div');
+    tarea.textContent = "Esquema";
+
+    tarea.className = "nuevatarea";
+
+    let contenedor = document.getElementById("contenedor");
+    contenedor.appendChild(tarea); 
+    console.log("Creaste una tarea tipo Esquema");  
+}
+function agregarpersonal(){
+
+    let tarea = document.createElement('div');
+    tarea.innerHTML = '<img src="../Statics/media/img/argentiros.jpg" class="item-img">';
+
+    tarea.className = "nuevatarea";
+
+    let contenedor = document.getElementById("contenedor");
+    contenedor.appendChild(tarea); 
+    console.log("Añadiste una Imagen");  
+}
 function eliminartarea(){
 
     let contenedor = document.getElementById("contenedor");
@@ -20,3 +85,13 @@ function eliminartarea(){
         console.log("Borraste una tarea :D");
     }   
 }
+
+info=document.getElementById("info");
+    toggle=document.getElementById("toggle");
+ 
+    toggle.addEventListener("click",()=>{
+        isHidden=info.style.display==="none";
+        info.style.display=isHidden? "block":"none";
+        toggle.textContent=isHidden? "Ocultar":"Tipos de tareas";
+        console.log("Se mostraron/ocultaron los tipos de tareas");
+    });
